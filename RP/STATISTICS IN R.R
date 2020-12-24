ca8de58e9ee6c6c3ec3d906d1c2be5716877ab7d
Adedamola Yusuf <adedamolayusuf@Adedamolas-MacBook-Pro.local>
2020-12-24 11:23:33 +0100

    update

--- RP/STATISTICS IN R.R
+++ RP/STATISTICS IN R.R
@@ -136,10 +136,129 @@ print(Xwt)
 #LOGICAL REGRESSION
 #Create Regression Model
 #We use the glm() function to create the regression model and get its summary for analysis.
-
 input <- mtcars[,c("am","cyl","hp","wt")]
 am.data = glm(formula = am ~ cyl + hp + wt, data = input, family = binomial)
 print(summary(am.data))
+
+#NORMAL DISTRIBUTION #we get a bell shaped curve
+#dnorm() This function gives height of the probability distribution at each point for a given mean and standard deviation.
+# Create a sequence of numbers between -10 and 10 incrementing by 0.1.
+x <- seq(-10, 10, by = .1)
+# Choose the mean as 2.5 and standard deviation as 0.5.
+y <- dnorm(x, mean = 2.5, sd = 0.5)
+# Give the chart file a name.
+png(file = "dnorm.png")
+plot(x,y)
+# Save the file.
+dev.off()
+
+#pnorm() This function gives the probability of a normally distributed random number to be less that the value of a given number. It is also called "Cumulative Distribution Function".
+# Create a sequence of numbers between -10 and 10 incrementing by 0.2.
+x <- seq(-10,10,by = .2)
+# Choose the mean as 2.5 and standard deviation as 2. 
+y <- pnorm(x, mean = 2.5, sd = 2)
+# Give the chart file a name.
+png(file = "pnorm.png")
+# Plot the graph.
+plot(x,y)
+# Save the file.
+dev.off()
+
+#qnorm() This function takes the probability value and gives a number whose cumulative value matches the probability value.
+# Create a sequence of probability values incrementing by 0.02.
+x <- seq(0, 1, by = 0.02)
+# Choose the mean as 2 and standard deviation as 3.
+y <- qnorm(x, mean = 2, sd = 1)
+# Give the chart file a name.
+png(file = "qnorm.png")
+# Plot the graph.
+plot(x,y)
+# Save the file.
+dev.off()
+
+#rnorm() This function is used to generate random numbers whose distribution is normal. It takes the sample size as input and generates that many random numbers. We draw a histogram to show the distribution of the generated numbers.
+# Create a sample of 50 numbers which are normally distributed.
+y <- rnorm(50)
+# Give the chart file a name.
+png(file = "rnorm.png")
+# Plot the histogram for this sample.
+hist(y, main = "Normal DIstribution")
+# Save the file.
+dev.off()
+
+#R - Binomial Distribution
+#The binomial distribution model deals with finding the probability of success of an event which has only two possible outcomes in a series of experiments.
+#dbinom() This function gives the probability density distribution at each point.
+# Create a sample of 50 numbers which are incremented by 1.
+x <- seq(0,50,by = 1)
+# Create the binomial distribution.
+y <- dbinom(x,50,0.5)
+# Give the chart file a name.
+png(file = "dbinom.png")
+# Plot the graph for this sample.
+plot(x,y)
+# Save the file.
+dev.off()
+
+#pbinom() This function gives the cumulative probability of an event. It is a single value representing the probability.
+# Probability of getting 26 or less heads from a 51 tosses of a coin.
+x <- pbinom(26,51,0.5)
+print(x)
+
+#qbinom() This function takes the probability value and gives a number whose cumulative value matches the probability value.
+# How many heads will have a probability of 0.25 will come out when a coin
+# is tossed 51 times.
+x <- qbinom(0.25,51,1/2)
+print(x)
+
+#rbinom()This function generates required number of random values of given probability from a given sample.
+# Find 8 random values from a sample of 150 with probability of 0.4.
+x <- rbinom(8,150,.4)
+print(x)
+
+#R - Poisson Regression
+#Poisson Regression involves regression models in which the response variable is in the form of counts and not fractional numbers. 
+#For example, the count of number of births or number of wins in a football match series. Also the values of the response variables follow a Poisson distribution.
+input <- warpbreaks
+print(head(input))
+#Create Regression Model
+output <-glm(formula = breaks ~ wool+tension, data = warpbreaks,
+             family = poisson)
+print(summary(output))
+
+#ANCOVA 
+#We create a regression model taking "hp" as the predictor variable and "mpg" as the response variable taking into account the interaction between "am" and "hp".
+
+#Model with interaction between categorical variable and predictor variable
+input <- mtcars[,c("am","mpg","hp")]
+print(head(input))
+# Get the dataset.
+input <- mtcars
+# Create the regression model.
+result <- aov(mpg~hp*am,data = input)
+print(summary(result))
+
+
+#Model without interaction between categorical variable and predictor variable
+# Get the dataset.
+input <- mtcars
+# Create the regression model.
+result <- aov(mpg~hp+am,data = input)
+print(summary(result))
+
+#Comparing Two Models
+#Now we can compare the two models to conclude if the interaction of the variables is truly in-significant. For this we use the anova() function.
+# Get the dataset.
+input <- mtcars
+
+# Create the regression models.
+result1 <- aov(mpg~hp*am,data = input)
+result2 <- aov(mpg~hp+am,data = input)
+
+# Compare the two models.
+print(anova(result1,result2))
+
+
 
 
 
---
+++ cod5/cod5.R
@@ -0,0 +1,5 @@
+#Chapter 5
+#working with texts
+x <- 'hello world!'
+x
+is.character(x)
